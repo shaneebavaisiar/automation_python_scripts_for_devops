05a5af9a98aca15946d9e126cdd746b88d3712fd
shaneeba <shaneeba@gmail.com>
2025-05-02 12:08:17 +0530

    add updated automation script

--- git_auto_commit_push_script.py
+++ git_auto_commit_push_script.py
@@ -4,12 +4,16 @@ within your Python script.
 It allows your Python code to interact with the system shell and other programs—just 
 like if you were typing commands in a terminal or command prompt."""
 def git_commit_push(branch_name,commit_msg):
-    subprocess.run(["git","branch",branch_name],check=True)
-    subprocess.run(["git","checkout",branch_name],check=True)
-    print(f"you checked out into {branch_name}")
-    subprocess.run(["git","add","."],check=True)
-    subprocess.run(["git","commit","-m",commit_msg],check=True)
-    subprocess.run(["git","push","origin","feature"],check=True)
-    print("chages pushed to remote repository !!!")
+    try:
+        subprocess.run(["git","checkout","-b",branch_name],check=True)
+        print(f"you created a new branch {branch_name} and checked out into {branch_name}")
+    except:
+        subprocess.run(["git","checkout",branch_name],check=True)
+        print(f"you already have the branch and checked out into {branch_name}")
+    finally:
+        subprocess.run(["git","add","."],check=True)
+        subprocess.run(["git","commit","-m",commit_msg],check=True)
+        subprocess.run(["git","push","origin",branch_name],check=True)
+        print("chages pushed to remote repository !!!")
 
 git_commit_push("feature1","add updated automation script")
